895385e89c13e4b4519871942ab4edce3842d396
deveshmishra1986-sys <deveshmishra1986@gmail.com>
2026-09-19 23:33:03 +0530

    Update tokens.js

--- api/tokens.js
+++ api/tokens.js
@@ -1,25 +1,25 @@
-const URL =
-  "https://explorer.shardeum.org/api/v2/transactions/" +
-  "0x6d372e3d993ddb281c749811f0323b4da7e79a0e75ab81905c1c97aa2cd71f92/" +
-  "token-transfers";
+const TX =
+  "0x6d372e3d993ddb281c749811f0323b4da7e79a0e75ab81905c1c97aa2cd71f92";
 
 module.exports = async function handler(req, res) {
-
   try {
+    const url =
+      `https://explorer.shardeum.org/api/v2/transactions/${TX}/token-transfers`;
 
-    const response = await fetch(URL);
+    const response = await fetch(url);
 
     const text = await response.text();
 
-    return res.status(200).json({
-      httpStatus: response.status,
-      data: JSON.parse(text)
+    res.status(200).json({
+      status: response.status,
+      response: text
     });
 
   } catch (error) {
 
-    return res.status(500).json({
-      error: error.message
+    res.status(500).json({
+      error: error.message,
+      stack: error.stack
     });
   }
 };
