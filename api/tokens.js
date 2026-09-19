1af523939892d8e1ee1b10a3d08ef7881151afd0
deveshmishra1986-sys <deveshmishra1986@gmail.com>
2026-09-19 23:40:20 +0530

    Update tokens.js

--- api/tokens.js
+++ api/tokens.js
@@ -1,15 +1,29 @@
-export default function handler(req, res) {
-  return res.status(200).json({
-    ok: true,
-    message: "Tokens API is working",
-    items: [
-      {
-        address: "0x3Fe5fbBA8034762fDd8d3d3b3d7E788B9a12F04",
-        name: "Shantum",
-        symbol: "STM",
-        decimals: 18,
-        tradeCount: 1
-      }
-    ]
-  });
+const EXPLORER_API =
+  "https://explorer.shardeum.org/api/v2";
+
+const TX =
+  "0x6d372e3d993ddb281c749811f0323b4da7e79a0e75ab81905c1c97aa2cd71f92";
+
+export default async function handler(req, res) {
+  try {
+    const url =
+      `${EXPLORER_API}/transactions/${TX}/token-transfers`;
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    return res.status(200).json({
+      success: true,
+      status: response.status,
+      data
+    });
+
+  } catch (error) {
+
+    return res.status(500).json({
+      success: false,
+      error: error.message
+    });
+  }
 }
