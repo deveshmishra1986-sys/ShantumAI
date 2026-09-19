51989965c0752881f6b73791b4c55644e1d15b72
deveshmishra1986-sys <deveshmishra1986@gmail.com>
2026-09-20 01:07:14 +0530

    add

--- api/shm-price.js
+++ api/shm-price.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   return res.status(200).json({
     success: true,
-    test: "NEW SHM PRICE FILE",
-    version: "2026-09-20-TEST-01"
+    test: "SHM API IS WORKING",
+    version: "TEST-002"
   });
 }
