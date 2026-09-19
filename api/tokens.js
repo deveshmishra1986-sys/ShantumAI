e401464fbcfd0049a4f0a1330c9c243ddf4ce5b0
deveshmishra1986-sys <deveshmishra1986@gmail.com>
2026-09-19 23:30:07 +0530

    sad

--- api/tokens.js
+++ api/tokens.js
@@ -1,7 +1,25 @@
+const URL =
+  "https://explorer.shardeum.org/api/v2/transactions/" +
+  "0x6d372e3d993ddb281c749811f0323b4da7e79a0e75ab81905c1c97aa2cd71f92/" +
+  "token-transfers";
+
 module.exports = async function handler(req, res) {
-  return res.status(200).json({
-    ok: true,
-    message: "Vercel API is working",
-    time: new Date().toISOString()
-  });
+
+  try {
+
+    const response = await fetch(URL);
+
+    const text = await response.text();
+
+    return res.status(200).json({
+      httpStatus: response.status,
+      data: JSON.parse(text)
+    });
+
+  } catch (error) {
+
+    return res.status(500).json({
+      error: error.message
+    });
+  }
 };
